66dd452331ae116de3660f8403090eec9e3e04fd
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2023-09-27 00:44:28 +0700

    add route for update-cart

--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import user from './src/controller/user';
-
+import bodyParser from 'body-parser';
 const app: express.Express = express();
 
-app.use('/index',user)
+app.use(bodyParser.json());
+
+
+
+app.use('/user',user)
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 
+interface cartItem{
+    image: string,
+    name: string,
+    price: string
+}
+
 const router = express.Router();
 
 router.use((req, res, next) => {
@@ -14,5 +20,20 @@ router.get('/info', (req, res, next) =>{
         cart: 24
     }
     res.json(response);
+})
+
+router.post('/update-cart', (req, res, next) =>{
+    try{
+        const cartItems: cartItem[] = req.body;
+        const filtered = cartItems.filter((item, index) => {
+            return cartItems.findIndex(i => i.name === item.name) === index;
+          });
+        for(let cartItem of filtered){
+            console.log(cartItem);
+        }
+        res.status(200).json({ message: 'Received' }); 
+    }catch(err){
+        console.log(err);
+    }
 })
 export default router;
